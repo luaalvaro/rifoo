71d9bf973bca2d4d57a06dbc311c0bd4176c1af0
Luã Álvaro <luan.alc@hotmail.com>
2022-07-20 17:25:21 -0300

    Imagens sendo exibidas do supabase

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,12 +1,39 @@
 import { Flex, Text, Button } from '@chakra-ui/react'
 import Image from 'next/image'
-import Biscoito from '../assets/biscoito.jpg'
+import { useEffect, useState } from 'react'
+import supabase from '../services/supabase'
 
 interface IProductCard {
     data: Product
 }
 
 const ProductCard: React.FC<IProductCard> = ({ data }) => {
+
+    const [signedURL, setSignedURL] = useState("")
+
+    const getUrlImages = async () => {
+        try {
+
+            const path = data.product_image_url.split("products/")[1]
+
+            const { data: resData, error } = await supabase
+                .storage
+                .from('products')
+                .createSignedUrl(path, 3600)
+
+            if (error) throw error
+
+            if (!resData) return
+
+            setSignedURL(resData.signedURL)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getUrlImages()
+    }, [])
     return (
         <Flex
             direction="column"
@@ -18,11 +45,13 @@ const ProductCard: React.FC<IProductCard> = ({ data }) => {
             <Flex
                 gridGap="20px"
             >
-                <Image
-                    src={Biscoito}
-                    width="120px"
-                    height="120px"
-                />
+                {signedURL !== "" &&
+                    <Image
+                        src={signedURL}
+                        width="120px"
+                        height="120px"
+                    />
+                }
 
                 <Flex
                     direction="column"
