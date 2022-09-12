ec13a9ead86e11c4b381e94a72a2d3ad54fe9bf9
Luã Álvaro <luan.alc@hotmail.com>
2022-09-12 15:53:20 -0300

    Edição do pagamento no supabase

--- src/pages/api/payments/update.ts
+++ src/pages/api/payments/update.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import mercadopago from 'mercadopago'
 import moment from 'moment'
+import supabase from '../../../services/supabase'
 
 type Data = {
     message: string,
@@ -19,13 +20,40 @@ interface PaymentAction {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     try {
-        console.log(req.body)
-
         const { response } = await mercadopago
             .payment
             .findById(req.body.data.id)
 
-        console.log(response)
+        const {
+            id,
+            status,
+            status_detail,
+            description,
+            transaction_amount,
+            date_of_expiration,
+            payer,
+            point_of_interaction
+        } = response
+
+        try {
+            const { data, error } = await supabase
+                .from('payments')
+                .update({
+                    transaction_status: status,
+                })
+                .eq('transaction_id', id)
+                .single()
+
+            if (error)
+                throw error
+
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+            return res.status(404).json({ message: "Erro na edição do pagamento" })
+        }
+
     } catch (error) {
         console.log(error)
     }
