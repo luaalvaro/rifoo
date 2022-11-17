fa7aa0bc4563e0f271dba2d85f75efdcc97e69e0
Luã Álvaro <91550379+luaalvaroubi@users.noreply.github.com>
2022-11-17 19:43:33 -0300

    Responsividade header e hero ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,67 +24,71 @@ const Home = () => {
         >
             {/* HEADER */}
             <Flex
+                width="100%"
                 position="fixed"
 
                 top="0"
-
                 background="#fff"
                 zIndex={5}
-
                 boxShadow="0px 8px 12px rgba(0, 0, 0, 0.15)"
-
-                width="100%"
-                height="65px"
-                justify="space-between"
-                align="center"
                 px="30px"
             >
                 <Flex
-                    gridGap="64px"
+                    width="100%"
+                    maxWidth="1180px"
+                    mx="auto"
+                    height="65px"
+                    justify="space-between"
+                    align="center"
+
                 >
-                    <Logo />
-
                     <Flex
-                        display={['none', 'none', 'none', 'flex', 'flex']}
-                        fontSize="20px"
-                        fontWeight={500}
-                        color="#405090"
-
-                        gridGap="32px"
+                        gridGap="64px"
                     >
-                        <Link href="/#vantagens">
-                            <A>Home</A>
-                        </Link>
+                        <Logo />
 
-                        <Link href="/#vantagens">
-                            <A>Vantagens</A>
-                        </Link>
+                        <Flex
+                            display={['none', 'none', 'none', 'flex', 'flex']}
+                            fontSize="20px"
+                            fontWeight={500}
+                            color="#405090"
 
-                        <Link href="/#vantagens">
-                            <A>Planos</A>
-                        </Link>
+                            gridGap="32px"
+                        >
+                            <Link href="/#vantagens">
+                                <A>Home</A>
+                            </Link>
+
+                            <Link href="/#vantagens">
+                                <A>Vantagens</A>
+                            </Link>
+
+                            <Link href="/#vantagens">
+                                <A>Planos</A>
+                            </Link>
+                        </Flex>
                     </Flex>
-                </Flex>
 
-                <Flex
-                    width="max-content"
-                    gridGap="16px"
-                >
                     <Flex
-                        display={['none', 'none', 'flex', 'flex', 'flex']}
+                        width="max-content"
+                        gridGap="16px"
                     >
+                        <Flex
+                            display={['none', 'none', 'flex', 'flex', 'flex']}
+                        >
+                            <Button
+                                width='200px'
+                                label='Quero me cadastrar'
+                            />
+                        </Flex>
+
                         <Button
-                            width='200px'
-                            label='Quero me cadastrar'
+                            label='Login'
+                            variant='outline'
+                            width='100px'
+                            leftIcon={<BiUserCircle fontSize="24px" />}
                         />
                     </Flex>
-
-                    <Button
-                        label='Login'
-                        variant='outline'
-                        width='100px'
-                        leftIcon={<BiUserCircle fontSize="24px" />}
-                    />
                 </Flex>
             </Flex>
 
@@ -93,97 +97,116 @@ const Home = () => {
                 width="100%"
                 minHeight="max-content"
                 background="radial-gradient(208.7% 631.97% at -51.69% 185.49%, rgba(0, 34, 164, 0.2) 23.05%, rgba(255, 255, 255, 0) 100%), radial-gradient(135.09% 508.11% at 114.35% -15.02%, rgba(81, 110, 222, 0.65) 6.2%, rgba(255, 255, 255, 0) 50.43%), #FFFFFF;"
-
                 pt="65px"
                 px="30px"
-
-                direction={["column", "column", "column", "row-reverse", "row-reverse"]}
-                justify="space-between"
-                align="center"
             >
-                <Flex>
-                    <Flex
-                        display={['flex', 'flex', 'flex', 'none', 'none']}
-                        width={359}
-                        height={514}
-                    >
-                        <Image
-                            src="/hero.png"
-                            alt="Imagem de fundo"
+                <Flex
+                    width="100%"
+                    maxWidth="1180px"
+                    mx="auto"
+
+                    direction={["column", "column", "column", "row-reverse", "row-reverse"]}
+                    justify="space-between"
+                    align="center"
+                >
+                    <Flex>
+                        <Flex
+                            display={['flex', 'flex', 'flex', 'none', 'none']}
                             width={359}
                             height={514}
-                            quality={70}
-                        />
+                        >
+                            <Image
+                                src="/hero.png"
+                                alt="Imagem de fundo"
+                                width={359}
+                                height={514}
+                                quality={70}
+                            />
+                        </Flex>
+
+                        <Flex
+                            display={['none', 'none', 'none', 'flex', 'none']}
+                            width={948 / 1.75}
+                            height={868 / 1.75}
+                        >
+                            <Image
+                                src="/hero-desktop.png"
+                                alt="Imagem de fundo"
+                                width={948 / 1.75}
+                                height={868 / 1.75}
+                                quality={70}
+                            />
+                        </Flex>
+
+                        <Flex
+                            display={['none', 'none', 'none', 'none', 'flex']}
+                            width={948 / 1.45}
+                            height={868 / 1.45}
+                        >
+                            <Image
+                                src="/hero-desktop.png"
+                                alt="Imagem de fundo"
+                                width={948 / 1.45}
+                                height={868 / 1.45}
+                                quality={70}
+                            />
+                        </Flex>
                     </Flex>
 
                     <Flex
-                        display={['none', 'none', 'none', 'flex', 'flex']}
-                        width={948 / 1.5}
-                        height={868 / 1.5}
-                    >
-                        <Image
-                            src="/hero-desktop.png"
-                            alt="Imagem de fundo"
-                            width={948 / 1.5}
-                            height={868 / 1.5}
-                            quality={70}
-                        />
-                    </Flex>
-                </Flex>
-
-                <Flex
-                    direction="column"
-                >
-                    <Flex
-                        width="100%"
-                        maxWidth={['initial', 'initial', 'initial', '580px', '580px']}
-                        mt="40px"
                         direction="column"
-                        gridGap="15px"
-                        textAlign={["center", "center", "center", "left", "left"]}
-                        color="#505050"
-                    >
-                        <Heading
-                            fontFamily="Poppins"
-                            textTransform="uppercase"
-                            fontSize={['24px', '24px', '24px', '32px', '36px']}
-                        >
-                            (PDV) Frente de Caixa p/<br />Vendedores Ambulantes
-                        </Heading>
-
-                        <Text
-                            color="#818181"
-                            fontSize={['15px', '15px', '15px', '20px', '24px']}
-                        >
-                            Com o Rifoo, você tem o seu negócio na palma da sua mão.
-                            Faça o seu cadastro e comece a vender agora mesmo!
-                        </Text>
-                    </Flex>
-
-                    <Flex
-                        mt="30px"
-                        mb="60px"
-                        gridGap="15px"
-                        direction={['column', 'column', 'column', 'row', 'row']}
                     >
                         <Flex
-                            maxWidth={['100%', '100%', '100%', '140px', '140px']}
+                            width="100%"
+                            maxWidth={['initial', 'initial', 'initial', '580px', '580px']}
+                            mt={["40px", "40px", "40px", "0", "0"]}
+                            direction="column"
+                            gridGap="15px"
+                            textAlign={["center", "center", "center", "left", "left"]}
+                            color="#505050"
                         >
-                            <Button
-                                label='Login'
-                                variant='outline'
-                                leftIcon={<BiUserCircle fontSize="24px" />}
-                            />
+                            <Heading
+                                fontFamily="Poppins"
+                                textTransform="uppercase"
+                                fontSize={['24px', '24px', '24px', '32px', '36px']}
+                            >
+                                (PDV) Frente de Caixa p/<br />Vendedores Ambulantes
+                            </Heading>
+
+                            <Text
+                                color="#818181"
+                                fontSize={['15px', '15px', '15px', '20px', '24px']}
+                            >
+                                Com o Rifoo, você tem o seu negócio na palma da sua mão.
+                                Faça o seu cadastro e comece a vender agora mesmo!
+                            </Text>
                         </Flex>
 
                         <Flex
-                            maxWidth={['100%', '100%', '100%', '180px', '180px']}
+                            mt="30px"
+                            mb="60px"
+                            gridGap="15px"
+                            direction={['column', 'column', 'column', 'row', 'row']}
                         >
-                            <Button
-                                label='Quero me cadastrar'
-                            />
-                        </Flex>
+                            <Flex
+                                maxWidth={['100%', '100%', '100%', '140px', '140px']}
+                            >
+                                <Button
+                                    label='Login'
+                                    variant='outline'
+                                    leftIcon={<BiUserCircle fontSize="24px" />}
+                                />
+                            </Flex>
 
+                            <Flex
+                                maxWidth={['100%', '100%', '100%', '180px', '180px']}
+                            >
+                                <Button
+                                    label='Quero me cadastrar'
+                                />
+                            </Flex>
+
+                        </Flex>
                     </Flex>
                 </Flex>
             </Flex>
