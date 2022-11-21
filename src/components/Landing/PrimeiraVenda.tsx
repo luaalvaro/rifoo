f463a157e8f6541b8d21f0577819c6412a299eb8
Luã Álvaro <91550379+luaalvaroubi@users.noreply.github.com>
2022-11-21 15:37:01 -0300

    Responsividade sec primeira venda primeira de mão finalizada

--- src/components/Landing/PrimeiraVenda.tsx
+++ src/components/Landing/PrimeiraVenda.tsx
@@ -9,12 +9,16 @@ const PrimeiraVenda = () => {
     return (
         <Flex
             width="100%"
-            paddingTop="140px"
+            // paddingTop="140px"
             direction="column"
+            // height="100%"
+            // minHeight="1650px"
+            // borderBottom={"1px solid black"}
 
             overflowX="hidden"
 
-            boxShadow={[shadow, shadow, shadow, shadow, "none"]}
+            boxShadow={[shadow, shadow, shadow, "none", "none"]}
+            p={["140px 0 0 0", "140px 0 0 0", "140px 0 0 0", "140px 0 140px 0", "140px 0 140px 0"]}
         >
             <Flex
                 display={['flex', 'flex', 'flex', 'flex', 'none']}
@@ -23,8 +27,8 @@ const PrimeiraVenda = () => {
             >
                 <Heading
                     color="#27242456"
-                    fontSize="60px"
-                    lineHeight="60px"
+                    fontSize={["60px", "60px", "60px", "80px", "80px"]}
+                    lineHeight={["60px", "60px", "60px", "80px", "80px"]}
                     fontWeight={900}
                     fontFamily="Poppins"
                 >
@@ -37,6 +41,8 @@ const PrimeiraVenda = () => {
                 marginTop={["40px", "40px", "40px", "100px", "100px"]}
                 minHeight="450px"
 
+                pr={[0, 0, 0, "30px", "30px"]}
+
                 gridGap="40px"
             >
                 <Flex
@@ -48,7 +54,7 @@ const PrimeiraVenda = () => {
                     borderRightRadius={['0px', '0px', '0px', '60px', '60px']}
                     position="relative"
 
-                    justify="flex-end"
+                    justify="center"
                     align="center"
                 >
                     <Flex
@@ -58,7 +64,7 @@ const PrimeiraVenda = () => {
                         mx={['auto', 'auto', 'auto', '0', '0']}
                         mt={["-50px", "-50px", "-50px", "initial", "initial"]}
 
-                        left={["0px", "0px", "0px", "12%", "12%"]}
+                        left={["0px", "0px", "0px", "8%", "0"]}
                     >
                         <Image
                             src="/celular01.png"
@@ -85,11 +91,12 @@ const PrimeiraVenda = () => {
                     </Flex>
 
                     <Flex
-                        width={["100%", "100%", "100%", "45%", "45%"]}
+                        width={["100%", "100%", "100%", "100%", "100%"]}
                         mt="15px"
                         mb="40px"
                         direction="column"
                         px="30px"
+                        ml={[0, 0, 0, "550px", "450px"]}
                     >
                         <Heading
                             width="100%"
@@ -97,18 +104,19 @@ const PrimeiraVenda = () => {
                             mx="auto"
                             textTransform="uppercase"
                             textAlign={["center", "center", "center", "left", "left"]}
-                            fontSize="26px"
+                            fontSize={["26px", "26px", "26px", "32px", "32px"]}
                         >
                             Cadastre seu produto
                         </Heading>
 
                         <Text
+                            width="100%"
                             color="#fff"
                             opacity={0.7}
-                            textAlign={["center", "center", "center", "left", "left"]}
+                            textAlign={["center", "center", "center", "justify", "justify"]}
                             mt="10px"
-                            fontSize="14px"
-                            maxWidth={["100%", "100%", "100%", "85%", "85%"]}
+                            fontSize={["14px", "14px", "14px", "18px", "18px"]}
+                            maxWidth={["100%", "100%", "100%", "100%", "100%"]}
                         >
                             Cadastrar um produto no RIFOO é tão simples quanto responder 4 perguntas. Em menos de 01 minuto você cadastra o seu primeiro produto e já está apto para começar a registrar as suas vendas.
                         </Text>
@@ -116,6 +124,7 @@ const PrimeiraVenda = () => {
                 </Flex>
 
                 <Flex
+                    minWidth={424}
                     display={['none', 'none', 'none', 'none', 'flex']}
                     maxWidth={300}
                     align="center"
@@ -133,6 +142,117 @@ const PrimeiraVenda = () => {
             </Flex>
 
             <Flex
+                width="100%"
+                marginTop={["0", "0", "0", "300px", "300px"]}
+                minHeight="450px"
+
+                pl={[0, 0, 0, "30px", "30px"]}
+
+                justify="flex-end"
+            >
+                <Flex
+                    minWidth="max-content"
+                    display={['none', 'none', 'none', 'none', 'flex']}
+                    maxWidth={300}
+                    align="center"
+                    pr="30px"
+                >
+                    <Heading
+                        color="#27242456"
+                        fontSize="90px"
+                        lineHeight="90px"
+                        fontWeight={900}
+                        fontFamily="Poppins"
+                    >
+                        EM DOIS<br /> PASSOS
+                    </Heading>
+                </Flex>
+
+                <Flex
+                    maxWidth="1180px"
+                    background="#5465A7"
+
+                    direction={['column', 'column', 'column', 'row', 'row']}
+
+                    borderLeftRadius={['0px', '0px', '0px', '60px', '60px']}
+                    position="relative"
+                    // justifySelf="flex-end"
+
+                    justify="center"
+                    align="center"
+                >
+                    <Flex
+                        position={["relative", "relative", "relative", "absolute", "absolute"]}
+                        width={[508 / 1.6, 508 / 1.6, 508 / 1.6, 508 / 1.2, 508 / 1.2]}
+                        height={[845 / 1.8, 845 / 1.8, 845 / 1.8, 845 / 1.3, 845 / 1.3]}
+                        mx={['auto', 'auto', 'auto', '0', '0']}
+                        mt={["30px", "30px", "30px", "initial", "initial"]}
+
+                        left={["0px", "0px", "0px", "8%", "20px"]}
+                    >
+                        <Image
+                            src="/celular02.png"
+                            alt="Celular acessando o Rifoo"
+                            layout="fill"
+                            style={{
+                                zIndex: 1
+                            }}
+                        />
+
+                        <Flex
+                            position="absolute"
+                            left={["-10", "-10", "-10", "90%", "90%"]}
+                            bottom={["-4", "-4", "-4", "28%", "28%"]}
+                        >
+                            <Text
+                                fontFamily="Poppins"
+                                fontSize={["200px", "200px", "200px", "300px", "300px"]}
+                                lineHeight={["200px", "200px", "200px", "300px", "300px"]}
+                                fontWeight={900}
+                                color="#fff"
+                                opacity="0.3"
+                            >
+                                2
+                            </Text>
+                        </Flex>
+                    </Flex>
+
+                    <Flex
+                        width={["100%", "100%", "100%", "100%", "100%"]}
+                        mt="15px"
+                        mb="40px"
+                        direction="column"
+                        px="30px"
+                        ml={[0, 0, 0, "650px", "570px"]}
+                    >
+                        <Heading
+                            width="100%"
+                            color="#fff"
+                            mx="auto"
+                            textTransform="uppercase"
+                            textAlign={["center", "center", "center", "left", "left"]}
+                            fontSize={["26px", "26px", "26px", "32px", "32px"]}
+                        >
+                            Cadastre seu produto
+                        </Heading>
+
+                        <Text
+                            width="100%"
+                            color="#fff"
+                            opacity={0.7}
+                            textAlign={["center", "center", "center", "justify", "justify"]}
+                            mt="10px"
+                            fontSize={["14px", "14px", "14px", "18px", "18px"]}
+                            maxWidth={["100%", "100%", "100%", "100%", "100%"]}
+                        >
+                            Cadastrar um produto no RIFOO é tão simples quanto responder 4 perguntas. Em menos de 01 minuto você cadastra o seu primeiro produto e já está apto para começar a registrar as suas vendas.
+                        </Text>
+                    </Flex>
+                </Flex>
+            </Flex>
+
+            <Flex
+                display="none"
                 mt={[0, 0, 0, 100, 200]}
                 ml="auto"
                 maxWidth="1180px"
@@ -232,7 +352,7 @@ const PrimeiraVenda = () => {
                     </Heading>
                 </Flex>
             </Flex>
-        </Flex>
+        </Flex >
     )
 }
 
