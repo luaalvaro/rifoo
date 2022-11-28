54de815f5655130f8d4713d15d6f7f8665812747
Luã Álvaro <91550379+luaalvaroubi@users.noreply.github.com>
2022-11-28 20:48:08 -0300

    Alterações no header config

--- src/components/HeaderConfig.tsx
+++ src/components/HeaderConfig.tsx
@@ -4,9 +4,10 @@ import Head from 'next/head'
 const HeaderConfig = () => {
     return (
         <Head>
-            <title>Rifoo - Seu negócio na palma da sua mão</title>
+            <title>Rifoo - PDV Frente de caixa para vendedores ambulantes</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta property="og:title" content="Rifoo - Seu negócio na palma da sua mão" key="title" />
+            <meta property="og:title" content="PDV Frente de caixa para vendedores ambulantes" key="title" />
+            <meta property="og:description" content="Com o Rifoo, você tem o seu negócio na palma da sua mão. Faça o seu cadastro e comece a vender agora mesmo!" key="description" />
         </Head>
     )
 }
